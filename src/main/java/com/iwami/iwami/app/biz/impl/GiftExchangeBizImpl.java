20bd76d4480e25727703d0ffa5f88cc59b47ce7b
verphen <verphen@163.com>
2014-02-26 14:10:35 +0800

    GiftExchangeBizImpl

--- src/main/java/com/iwami/iwami/app/biz/impl/GiftExchangeBizImpl.java
+++ src/main/java/com/iwami/iwami/app/biz/impl/GiftExchangeBizImpl.java
@@ -31,41 +31,33 @@ public class GiftExchangeBizImpl implements GiftExchangeBiz {
 		List<Exchange> list = new ArrayList<Exchange>();
 		for(int i=0;i<ids.length;i++){
 			Exchange ex = new Exchange();
-			ex.setUserid(userid);
-			ex.setPresentid(ids[i]);
-//			ex.setPresentName(presentGiftService.getUser(cellPhone).getName());
+			ex.setUserid(userid); // ---
+			ex.setPresentid(ids[i]); // ---
+			ex.setPresentName(giftExchangeService.getPresent(ids[i]).getName());
 			ex.setPresentPrize(-1);
-			ex.setPresentType(5);
-			ex.setCount(-1);
-			ex.setPrize(prize);
+			ex.setPresentType(giftExchangeService.getPresent(ids[i]).getPrize());
+			ex.setCount(count[i]);  
+			ex.setPrize(giftExchangeService.getPresent(ids[i]).getPrize()*count[i]);	 
 			ex.setStatus(0);
-			ex.setCellPhone(0);
-			ex.setAlipayAcount(null);
-			ex.setBankAcount(0);
-			ex.setBankName(null);
-			ex.setAddress(null);
-			ex.setName(null);
+			ex.setCellPhone(cellPhone);   
+			ex.setAlipayAcount(aliAccount);  
+			ex.setBankAcount(bankAccount); 
+			ex.setBankName(bankName);	 
+			ex.setAddress(address);		 
+			ex.setName(name);		 
 			ex.setExpress(null);
 			ex.setLastmodTime(System.currentTimeMillis());
-			ex.setLastmodUserid((int)userid);
+			ex.setLastmodUserid(0);
 			ex.setIsdel(0);
-			
-			//TODO  接着该处继续做 批量插入
-			
-			
 			list.add(ex);
 		}
 		giftExchangeService.addExchangeList(list);
 		
-		
-		
-		
-		//从user表current_prize减去米粒数，exchange加上米粒数
 		giftExchangeService.subCurrPrize(userid, prize);
 		giftExchangeService.addPrize(userid, prize);
 		
-		
 		//以上代码添加事务
+		
 		return false;
 	}
 
