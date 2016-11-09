37dbb24acaca6b6191e5a19446c4954fdbc1f754
effine <zhangyafei@co-mall.com>
2016-11-09 14:19:20 +0800

    update

--- src/test/java/cn/effine/Test.java
+++ src/test/java/cn/effine/Test.java
@@ -1,22 +1,18 @@
 package cn.effine;
 
-import org.springframework.beans.BeanUtils;
-
 public class Test {
 
 	public static void main(String[] args) {
-		String s1 = "helloword";
-		String s2 = new String("helloword");
-		String s3 = "hello" + "word";
-		String s4 = new String("hello") + "word";
+		// BeanUtils.copyProperties("", "");
 
-		System.out.println(s1 == s2); // false
-		System.out.println(s1 == s3); // true
-		System.out.println(s1 == s4); // false
-		System.out.println(s2 == s4); // false
-		
-		String.valueOf(s1);
-		s1.toString();
-		BeanUtils.copyProperties("", "");
+		String s0 = "kvill";
+
+		String s1 = "kvill";
+
+		String s2 = "kv" + "ill";
+
+		System.out.println(s0 == s1);
+		System.out.println(s0 == s2);
+
 	}
 }
